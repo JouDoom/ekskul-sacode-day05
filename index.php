f196d2b10f7977748478786c12d9e11677d90434
Jou Doom <91768022+JouDoom@users.noreply.github.com>
2022-03-19 09:21:57 +0900

    add loopdata

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foreach Loop - Jou Doom</title>
+</head>
+
+<body>
+<?php 
+    //loop
+
+    //While Loop
+    //For Loop
+    //Foreach Loop
+    $array = array ("Sonny", "Ezra", "Jou","John","Mariane");
+
+    foreach ($array as $loopdata) {
+        echo "My name is ".$loopdata."<br>";
+    }
+
+
+
+?>
+
+
+    
+</body>
+
+</html>
